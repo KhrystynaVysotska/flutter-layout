a2fcc190bd4570fc9366eea45b135818d314b529
Khrystyna <khrystynavysotska@gmail.com>
2021-09-30 00:14:01 +0300

    Created file with icon widgets

---
+++ lib/assets/icons.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+Widget starIcon = Icon(
+  Icons.star,
+  size: 15.0,
+  color: Colors.black.withOpacity(0.6),
+);
+
+Widget bookmarkIcon = Icon(
+  Icons.bookmark_border,
+  size: 20.0,
+  color: Colors.black.withOpacity(0.6),
+);
+
+Widget arrowBackIcon = const Icon(
+  Icons.arrow_back,
+  semanticLabel: 'Return',
+);
