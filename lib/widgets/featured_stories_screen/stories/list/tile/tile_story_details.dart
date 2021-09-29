01a8fe2fcb96e36d177452d7f08749e9030146f2
Khrystyna <khrystynavysotska@gmail.com>
2021-09-30 00:33:49 +0300

    Story details on list item: author, reading time, when was written

---
+++ lib/widgets/featured_stories_screen/stories/list/tile/tile_story_details.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_layout/assets/icons.dart';
+import 'package:flutter_layout/assets/constants.dart';
+
+class TileStoryDetails extends StatelessWidget {
+  const TileStoryDetails({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget storyDetailsAuthor = Container(
+        padding: const EdgeInsets.only(bottom: 1),
+        child: const Text(FAKE_STORY_AUTHOR));
+
+    Widget storyDetailsTime = Row(
+      children: <Widget>[
+        Text(
+          FAKE_STORY_TIME_DETAILS,
+          style: TextStyle(fontSize: 13, color: Colors.black.withOpacity(0.6)),
+        ),
+        starIcon
+      ],
+    );
+
+    Widget storyDetails = Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[storyDetailsAuthor, storyDetailsTime],
+    );
+
+    Widget saveStory = Container(
+        padding: const EdgeInsets.only(right: 10), child: bookmarkIcon);
+
+    return Container(
+        padding: const EdgeInsets.only(top: 18),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[storyDetails, saveStory],
+        ));
+  }
+}
