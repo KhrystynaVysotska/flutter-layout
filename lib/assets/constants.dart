0db59ff5763f9a2b3797ccc7fec88c79b48ce03a
Khrystyna <khrystynavysotska@gmail.com>
2021-09-30 00:12:33 +0300

    Created file to store constant values

---
+++ lib/assets/constants.dart
@@ -0,0 +1,24 @@
+// App
+const double APP_BAR_HEIGHT = 107.0;
+const String APP_TITLE = "Medium app screen layout";
+
+// Images
+const int NETWORK_IMAGES_MAX_ID = 1084;
+const String NETWORK_IMAGES_URL = "https://picsum.photos";
+
+// Fake stories
+const String FAKE_STORY_TITLE = 'The Story We Tell Ourselves';
+const String FAKE_STORY_DESCRIPTION = 'How do you see luck?';
+const String FAKE_STORY_AUTHOR = 'Darren Matthews';
+const String FAKE_STORY_TIME_DETAILS = '6 hours ago  -  2 min read  ';
+
+// Card
+const int CARD_IMAGE_WIDTH = 350;
+const int CARD_IMAGE_HEIGHT = 95;
+
+// List item
+const int LIST_ITEM_IMAGE_WIDTH = 80;
+const int LIST_ITEM_IMAGE_HEIGHT = 80;
+
+// CAROUSEL
+const double CAROUSEL_HEIGHT = 256.0;
