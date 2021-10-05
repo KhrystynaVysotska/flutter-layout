bdc72d70e6d80197580bd83ee674c0a38605b7df
Khrystyna <khrystynavysotska@gmail.com>
2021-10-05 19:58:45 +0300

    Changed type to Icon instead of Widget

--- lib/assets/icons.dart
+++ lib/assets/icons.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 
-Widget starIcon = Icon(
+Icon starIcon = Icon(
   Icons.star,
   size: 15.0,
   color: Colors.black.withOpacity(0.6),
 );
 
-Widget bookmarkIcon = Icon(
+Icon bookmarkIcon = Icon(
   Icons.bookmark_border,
   size: 20.0,
   color: Colors.black.withOpacity(0.6),
 );
 
-Widget arrowBackIcon = const Icon(
+Icon arrowBackIcon = const Icon(
   Icons.arrow_back,
   semanticLabel: 'Return',
 );
